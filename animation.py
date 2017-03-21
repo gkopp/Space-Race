52a6a8ccbe35f114e70a3c1277e066e1de3a7e4f
Grace E Kopp <gkopp@student02.cse.nd.edu>
2017-03-21 15:54:26 -0400

    add better comments

--- animation.py
+++ animation.py
@@ -2,15 +2,30 @@
 
 from visual import *
 
-earth = sphere (pos=(2,4,0), radius=.5, color=color.blue)
-sun = sphere (pos=(0,0,0), radius=1, color=color.orange)
+# initialize "earth" object attributes (arbitrary for now)
+earth = sphere()
+earth.pos = vector(2,4,0)
+earth.radius = .5
+earth.color = color.blue
+
+#initialize "sun" object attributes (arbitrary for now)
+sun = sphere()
+sun.pos = vector(0,0,0)
+sun.radius = 1
+sun.color = color.orange
+
+# set initial velocity as downwards
 earth.velocity = vector(0,-1,0)
 dt = 0.01
 
-while 1:
-    rate (100)
+while 1:                         # run forever
+    rate (100)                   # specifies max executions per second
+
+    # caluclate new position with  distance = prev distance + rate*time
     earth.pos = earth.pos + earth.velocity*dt
-    if earth.y < earth.radius:
+
+    if earth.y < earth.radius:                        # bounce back up
         earth.velocity.y = abs(earth.velocity.y)
     else:
+        # new velocity from equation v = v0 + a*time where a is -9.8 (gravity)
         earth.velocity.y = earth.velocity.y - 9.8*dt
