8c330634f9b9b31d47eb898e1a06a3de7f009cc0
Grace E Kopp <gkopp@darrow3.cc.nd.edu>
2017-04-16 13:19:47 -0400

    add remaining planets

--- orbit.py
+++ orbit.py
@@ -3,15 +3,39 @@
 from visual import *
 from visual.controls import *
 
+solar_system = display(title='Solar System',width=700, height=700,
+    x=0, y=0)
+
 # Define Sun/Planet attributes
-sun = sphere(pos=(0,0,0), radius=7e10, color=color.yellow)
-mercury = sphere(pos=(1e11,1e11,0), radius=2e10, color=(.750,.750,.750))
-earth = sphere(pos=(2.5e11,1.5e11,0), radius=3.5e10, color=(.408,.545,.769))
-jupiter = sphere(pos=(5.5e11,4e11,0), radius=5e10, color=(.847,.620,.570))
+sun = sphere(pos=(0,0,0), radius=3, color=color.yellow)
+
+# Planets from largest to smallest
+jupiter = sphere(radius=1.5,color=(.847,.620,.570))
+jupiter.pos = (sun.radius+jupiter.radius+5, sun.radius+jupiter.radius+5,0)
+
+saturn = sphere(radius = 1.25, color =(.878,.949,.989))
+saturn.pos = (sun.radius+saturn.radius+10,sun.radius+saturn.radius+10,0)
+uranus = sphere(radius = 1, color =(0,.663,1))
+uranus.pos = (sun.radius+19+uranus.radius,sun.radius+uranus.radius+19,0)     
+
+neptune = sphere(radius = .8, color =color.blue)
+neptune.pos = (sun.radius+neptune.radius+30, sun.radius+neptune.radius+30,0)
+
+earth = sphere(radius=.5, color=(.408,.545,.769))
+earth.pos = (sun.radius+earth.radius+1.5,sun.radius+earth.radius+1.5,0)
+
+venus = sphere(radius = .4, color=color.red)
+venus.pos = (sun.radius+venus.radius+.8,sun.radius+venus.radius+.8,0)
+
+mars = sphere(radius = .3, color =color.orange)
+mars.pos = (sun.radius+mars.radius+2.5,sun.radius+mars.radius+2.5,0)
+
+mercury = sphere(radius=.2, color=(.750,.750,.750))
+mercury.pos = (sun.radius+mercury.radius+0.3,sun.radius+mercury.radius+0.3,0)
 
 # create lists of planets and corresponding relative speeds
-speeds = [1, .75, .5]
-planets = [mercury, earth, jupiter]
+speeds = [1,.39, .24,.13,.02,.008,.003,.001]
+planets = [mercury,mars,venus,earth,jupiter,saturn,uranus,neptune]
 
 # initialize trail for each planet
 for planet in planets:
